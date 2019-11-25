e2e089f0ae7da8bac655b0dd7074c4fc2eb7a3b3
Samar-not-a-coder <pirsamarhussainradhan@gmail.com>
2019-11-26 02:13:18 +0500

    added some list examples

---
+++ afterHello.hs
@@ -0,0 +1,26 @@
+--main = print ("hi")
+listOfSomeNumbers = [1,2,3,4,5]
+main = do
+       print (head listOfSomeNumbers)
+       print (tail listOfSomeNumbers)
+       putStrLn (show (listOfSomeNumbers !! 2))
+       print (take 3 listOfSomeNumbers)
+       print (drop 3 listOfSomeNumbers)
+       print (length listOfSomeNumbers)
+       print (sum listOfSomeNumbers)
+       print (product listOfSomeNumbers)
+       print ("Couldn't print the added lists")
+       print (reverse listOfSomeNumbers)
+
+-- head [1,2,3,4,5]
+-- tail [1,2,3,4,5]
+--[1,2,3,4,5] !! 2
+--take 3 [1,2,3,4,5]
+--what are higher order functions like map, filter and fold
+--drop 3 [1,2,3,4,5]
+--length [1,2,3,4,5]
+--sum [1,2,3,4,5]
+--product [1,2,3,4,5]
+-- [1,2,3]+[4,5]
+--reverse [1,2,3,4,5]
+--f a +b means f(a)+b i.e functions have more precedence than addition
