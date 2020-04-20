66a2e379e6b582665c2a31c533c599084fc261cf
Samar-not-a-coder <pirsamarhussainradhan@gmail.com>
2020-04-20 23:02:19 +0500

    added the example of factorial

--- topic1.hs
+++ topic1.hs
@@ -25,7 +25,33 @@ addMe x y = x + y
 --making a function without declaring the type can make it use multiple types for parameters
 --which can be a useful thing but can also cause problems/unintended effects
 sumMe x y = x + y
---start from 33 minutes, time for a break
+
+--add tuples
+addTuples :: (Int,Int) -> (Int,Int) -> (Int,Int)
+addTuples (x, y) (x2, y2) = (x+x2, y +y2)
+--note that this one only receives tuples of size 2
+
+--using conditions with functions
+drivingAge  :: Int -> String
+drivingAge 18 = "You can drive"
+drivingAge 80 = "You are too old"
+--now the above one will work with only two of those values
+--u can use x or _ to make it work with other values
+drivingAge _= "I don't about that one"
+--drivingAge x= "I don't know about that one"
+
+--using recursion, example of factorial
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+--Every recursion needs a breaking point/case to stop it, otherwise it will keep on going forever
+--Here the breaking point is 0.
+--Think of it as an if condition.
+--When the value passed is 0, give 1. When it is not 1, do n * factorial (n-1)
+--and now factorial calls itself until the value is finally 0.
+--what I don't understand here is that how does it not process negative values. like is there a built-in safety system?
+--how to make a function process negative numbers?
+
 
 --main function
 main = do
