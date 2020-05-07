a82d2ad0bf54cc48028df7272df15a46edcec356
Samar-not-a-coder <pirsamarhussainradhan@gmail.com>
2020-05-08 00:24:16 +0500

    added some more questions

--- someFunctions.hs
+++ someFunctions.hs
@@ -52,4 +52,47 @@ main = do
 --Haskell also provides tuples. Tuples have a fixed length, but elements of different types can be combined.
 --such (1, "Hello")
 
+--what is type inference?
+--how does this expression work?
+--e :: t
+--why does every expression have a type in Haskell?
+--in GHCi the :type command calculates the type of an expression without evaluating it.
 
+--Int is fixed precision integer
+--Integer is arbitrary precision integer
+
+--why doesn't Integer overflow but Int does?
+--the type of a tuple indicates the length of the tuple too
+
+--how is that a function is a mapping from values of one type to values of another type?
+--t1 -> t2 is the type of functions that map values of type t1 to values of type t2.
+--in that case t1 is the domain of the function while t2 is the range.
+--what are domain and range in the above case?
+
+--how to know when a function is receiving two ints and returns an int and when a
+--function receives an int and returns a function that takes an int and returns and int
+--or in other words, how to know if the function is curried or not?
+
+--curried functions take the arguments one by one.
+--what is the difference between
+--add :: (Int, Int) -> Int
+--add :: Int -> (Int -> Int)
+--and what is the correct way to write the above type signatures?
+
+--how can every function be turned into a curried function?
+--how does partial application of function work in curried functions?
+
+--what is the reason to use curried functions?
+--in what case should one use a curried function over a non curried one?
+
+--how is this function a curried function?
+mult :: Int -> Int -> Int -> Int
+mult x y z = x * y * z
+
+--how is curried function different than method overloading and reusing?
+--for example someMethod(arg1){do something}, someMethod(arg1, arg2){someMethod(arg1), do something}
+
+--how is Haskell optimized for curried functions?
+
+
+--start from types and classes part 3
